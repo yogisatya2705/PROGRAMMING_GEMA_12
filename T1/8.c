fbd1b865e7f16199b8033e2cff57fd7c360f1651
yogisatya <yogisatya270599@gmail.com>
2019-08-23 11:43:06 +0800

    T1
    update 8.c

--- T1/8.c
+++ T1/8.c
@@ -5,43 +5,56 @@ int main()
   char p[26] = "abcdefghijklmnopqrstuvwxyz";
   char c[26] = "zyxwvutsrqponmlkjihgfedcba";
 
-  char kalimat[100], ch;
+  int n = 0;
+  scanf("%d", &n);
+  getchar();
 
-  scanf("%[^\n]s", &kalimat);
+  char kalimat[n][100], ch;
 
-  for (int i = 0; kalimat[i] != '\0'; i++)
+  for (int i = 0; i < n; i++)
   {
-    ch = kalimat[i];
+    fgets(kalimat[i], 100, stdin);
+  }
 
-    for (int j = 0; j < 26; j++)
+  for (int cn = 0; cn < n; cn++)
+  {
+    for (int i = 0; kalimat[cn][i] != '\0'; i++)
     {
-      if (ch == p[j])
-      {
-        ch = c[j];
-        j = 26;
-      }
-      else if (ch == '(')
-      {
-        if (kalimat[i + 1] == '\'')
-        {
-          for (int x = i + 1; kalimat[x] != '\0'; x++)
-          {
-            if (kalimat[x + 1] == '\'' && kalimat[x + 2] == ')')
-            {
-              i = x + 2;
-              ch = kalimat[i];
+      ch = kalimat[cn][i];
 
-              break;
+      for (int j = 0; j < 26; j++)
+      {
+        if (ch == p[j])
+        {
+          ch = c[j];
+          j = 26;
+        }
+        else if (ch == '(')
+        {
+          if (kalimat[cn][i + 1] == '\'')
+          {
+            for (int x = i + 1; kalimat[cn][x] != '\0'; x++)
+            {
+              if (kalimat[cn][x + 1] == '\'' && kalimat[cn][x + 2] == ')')
+              {
+                i = x + 2;
+                ch = kalimat[cn][i];
+
+                break;
+              }
             }
           }
         }
       }
-    }
 
-    kalimat[i] = ch;
+      kalimat[cn][i] = ch;
+    }
   }
 
-  printf("%s", kalimat);
+  for (int i = 0; i < n; i++)
+  {
+    printf("%s", kalimat[i]);
+  }
 
   return 0;
 }
